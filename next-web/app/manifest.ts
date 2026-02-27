981056b08719662359ce3dad727ce857d96cbe77
nakano-ebiz <tomosuke.nakano@gmail.com>
2026-02-27 11:08:45 +0900

    fix: マニフェスト用アイコンの追加とbasePathへの対応

--- next-web/app/manifest.ts
+++ next-web/app/manifest.ts
@@ -7,18 +7,18 @@ export default function manifest(): MetadataRoute.Manifest {
         name: 'Drip Logger',
         short_name: 'DripLogger',
         description: 'Log your coffee brewing recipes and results.',
-        start_url: '/',
+        start_url: '/drip-logger/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
         icons: [
             {
-                src: '/icon-192x192.png',
+                src: '/drip-logger/icon-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: '/icon-512x512.png',
+                src: '/drip-logger/icon-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
             },
